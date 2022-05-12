37b1b1420f49446b47ce87da866ded035749a51e
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-11 22:23:22 -0700

    add cors to allow access

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
 
 
 
@@ -10,7 +14,7 @@ app.listen(process.env.PORT || 5000, function (err) {
 
 
 app.get('/:id', function(req, res) {
-    id = req.params.id
+    let id = req.params.id;
     console.log("GET Request received");
     res.sendFile(__dirname+ `/${id}.json`);
   })
