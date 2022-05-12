a0ac6511fcd9519274323a85e9d56640516c0e6f
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-11 21:12:37 -0700

    fix bodyparser error on api server

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const mongoose = require('mongoose');
 
-// Connect to mongodb with mongoose module
-mongoose.connect("mongodb://localhost:27017/test",
- {useNewUrlParser: true, useUnifiedTopology: true});
-
-
-app.use(bodyparser.urlencoded({
-    extended: true
-  }));
 
 
 app.listen(process.env.PORT || 5000, function (err) {
