f8f1ccf6314391afb001c4848bc7c1dd5c5b2ee7
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-22 15:44:43 -0700

    remove obsolte codes from assignment 2

--- server.js
+++ server.js
@@ -23,52 +23,7 @@ mongoose.connect("mongodb+srv://kkjin0330:5VO1M61v9prYQEpp@cluster0.msyad.mongod
     useUnifiedTopology: true
 });
 
-const pokemonSchema = new mongoose.Schema({
-    abilities: Array,
-    base_experience: Number,
-    forms: Array,
-    game_indices: Array,
-    height: Number,
-    held_items: Array,
-    id: Number,
-    is_default: Boolean,
-    location_area_encounters: String,
-    moves: Array,
-    name: String,
-    order: Number,
-    species: {},
-    sprites: {},
-    stats: Array,
-    types: Array,
-    weight: Number
-})
-
-const typeSchema = new mongoose.Schema({
-    damage_relations: {},
-    game_indices: Array,
-    generation: Object,
-    id: Number,
-    move_damage_class: {},
-    moves: Array,
-    name: String,
-    names: Array,
-    past_damage_relations: Array,
-    pokemon: Array
-})
-
-const habitatSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    names: Array,
-    pokemon_species: Array,
-})
-
-
-const pokemonModel = mongoose.model("pokemon_by_ids", pokemonSchema);
-const typeModel = mongoose.model("pokemon_by_types", typeSchema);
-const habitatModel = mongoose.model("pokemon_by_habitats", habitatSchema);
-
-
+// Use body-parser
 app.use(bodyparser.urlencoded({
     parameterLimit: 100000,
     limit: '50mb',
@@ -76,45 +31,93 @@ app.use(bodyparser.urlencoded({
 }));
 
 
+
+
+// Obsolete codes from Assignment 2 -> Nabil no longer requires to hold poke API on this server
+// const pokemonSchema = new mongoose.Schema({
+//     abilities: Array,
+//     base_experience: Number,
+//     forms: Array,
+//     game_indices: Array,
+//     height: Number,
+//     held_items: Array,
+//     id: Number,
+//     is_default: Boolean,
+//     location_area_encounters: String,
+//     moves: Array,
+//     name: String,
+//     order: Number,
+//     species: {},
+//     sprites: {},
+//     stats: Array,
+//     types: Array,
+//     weight: Number
+// })
+
+// const typeSchema = new mongoose.Schema({
+//     damage_relations: {},
+//     game_indices: Array,
+//     generation: Object,
+//     id: Number,
+//     move_damage_class: {},
+//     moves: Array,
+//     name: String,
+//     names: Array,
+//     past_damage_relations: Array,
+//     pokemon: Array
+// })
+
+// const habitatSchema = new mongoose.Schema({
+//     id: Number,
+//     name: String,
+//     names: Array,
+//     pokemon_species: Array,
+// })
+
+
+// const pokemonModel = mongoose.model("pokemon_by_ids", pokemonSchema);
+// const typeModel = mongoose.model("pokemon_by_types", typeSchema);
+// const habitatModel = mongoose.model("pokemon_by_habitats", habitatSchema);
+
 // READ pokemon api with id from the server with GET Request
-app.get('/pokemon_by_id/:id', function (req, res) {
-    let pokemon_id = req.params.id
-    console.log(`Received a GET request for ${pokemon_id}`);
-    pokemonModel.find({'id': pokemon_id}, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Data " + data);
-        }
-        res.send(data);
-    });
-})
+// app.get('/pokemon_by_id/:id', function (req, res) {
+//     let pokemon_id = req.params.id
+//     console.log(`Received a GET request for ${pokemon_id}`);
+//     pokemonModel.find({'id': pokemon_id}, function (err, data) {
+//         if (err) {
+//             console.log("Error " + err);
+//         } else {
+//             console.log("Data " + data);
+//         }
+//         res.send(data);
+//     });
+// })
 
 // READ pokemon api with type_id from the server with GET Request
-app.get('/pokemon_by_type/:id', function (req, res) {
-    let type_id = req.params.id
-    console.log(`Received a GET request for ${type_id}`);
-    typeModel.find({'id': type_id}, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Data " + data);
-        }
-        res.send(data);
-    });
-})
+// app.get('/pokemon_by_type/:id', function (req, res) {
+//     let type_id = req.params.id
+//     console.log(`Received a GET request for ${type_id}`);
+//     typeModel.find({'id': type_id}, function (err, data) {
+//         if (err) {
+//             console.log("Error " + err);
+//         } else {
+//             console.log("Data " + data);
+//         }
+//         res.send(data);
+//     });
+// })
 
 
 // READ pokemon api with habitat_id from the server with GET Request
-app.get('/pokemon_by_habitat/:id', function (req, res) {
-    let habitat_id = req.params.id
-    console.log(`Received a GET request for ${habitat_id}`);
-    habitatModel.find({'id': habitat_id}, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Data " + data);
-        }
-        res.send(data);
-    });
-})
+// app.get('/pokemon_by_habitat/:id', function (req, res) {
+//     let habitat_id = req.params.id
+//     console.log(`Received a GET request for ${habitat_id}`);
+//     habitatModel.find({'id': habitat_id}, function (err, data) {
+//         if (err) {
+//             console.log("Error " + err);
+//         } else {
+//             console.log("Data " + data);
+//         }
+//         res.send(data);
+//     });
+// })
